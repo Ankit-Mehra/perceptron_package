2004a24a89c54ff06508a80d1690e5c10eb08053
Ankit Mehra <ankitmehra1986@gmail.com>
2022-02-11 15:05:32 -0500

    Update 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ USERNAME = "Ankit-Mehra"
 
 setuptools.setup(
     name=f"{PKG_NAME}-{USERNAME}",
-    version="0.0.2",
+    version="0.0.3",
     author=USERNAME,
     author_email="ankitmehra1986@gmail.com",
     description="A small example package for perceptron",
